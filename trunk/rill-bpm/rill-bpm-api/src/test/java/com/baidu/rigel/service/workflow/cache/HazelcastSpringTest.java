1ac5f4c66c8f3dac368dc5a52168bb6125fd7898
totyumengr@gmail.com <totyumengr@gmail.com@441bde5e-0c81-691e-53e5-807c429adba5>
2011-07-11 10:12:54 +0000

    Hazelcast cache testcase

--- trunk/rill-bpm/rill-bpm-api/src/test/java/com/baidu/rigel/service/workflow/cache/HazelcastSpringTest.java
+++ trunk/rill-bpm/rill-bpm-api/src/test/java/com/baidu/rigel/service/workflow/cache/HazelcastSpringTest.java
@@ -4,7 +4,11 @@
  */
 package com.baidu.rigel.service.workflow.cache;
 
+import com.hazelcast.core.EntryEvent;
+import com.hazelcast.core.EntryListener;
 import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
+import java.util.concurrent.BlockingQueue;
 import javax.annotation.Resource;
 import junit.framework.Assert;
 import org.junit.Test;
@@ -40,21 +44,91 @@ public class HazelcastSpringTest extends AbstractJUnit4SpringContextTests {
     }
     
     @Test
-    public void testTwoInstance() {
+    public void testTwoInstance() { 
         
         Assert.assertNotNull(hz1Instance);
         Assert.assertNotNull(hz2Instance);
         
+        // Map test ------------------------------------------------------------
+        // Entry listener configuration
+        IMap<String, Integer> hz1LRUMap = hz1Instance.getMap("hz1LRUMap");
+        hz1LRUMap.addEntryListener(new EntryListener<String, Integer>() {
+            
+            public void entryAdded(EntryEvent<String, Integer> event) {
+                logger.info(event.toString());
+            }
+
+            public void entryRemoved(EntryEvent<String, Integer> event) {
+                logger.info(event.toString());
+            }
+
+            public void entryUpdated(EntryEvent<String, Integer> event) {
+                logger.info(event.toString());
+            }
+
+            public void entryEvicted(EntryEvent<String, Integer> event) {
+                logger.info(event.toString());
+            }
+            
+        }, true);
+        
+        IMap<String, String> hz2LRUMap = hz2Instance.getMap("hz2LRUMap");
+        hz2LRUMap.addEntryListener(new EntryListener<String, String>() {
+            
+            public void entryAdded(EntryEvent<String, String> event) {
+                logger.info(event.toString());
+            }
+
+            public void entryRemoved(EntryEvent<String, String> event) {
+                logger.info(event.toString());
+            }
+
+            public void entryUpdated(EntryEvent<String, String> event) {
+                logger.info(event.toString());
+            }
+
+            public void entryEvicted(EntryEvent<String, String> event) {
+                logger.info(event.toString());
+            }
+            
+        }, true);
+        
         // Put operations
-        hz1Instance.getMap("hz1LRUMap").put("1", new Integer(1));
-        hz1Instance.getMap("hz1LRUMap").put("2", new Integer(2));
-        hz2Instance.getMap("hz2LRUMap").put("a", "a");
-        hz2Instance.getMap("hz2LRUMap").put("b", "b");
+        hz1LRUMap.put("1", new Integer(1));
+        hz1LRUMap.put("2", new Integer(2));
+        hz2LRUMap.put("a", "a");
+        hz2LRUMap.put("b", "b");
         
         // Cache Assert
         Assert.assertTrue(hz1Instance.getMap("hz1LRUMap").get("1").equals(new Integer(1)));
-        Assert.assertTrue(hz1Instance.getMap("hz1LRUMap").get("a").equals("a"));
-        Assert.assertTrue(hz2Instance.getMap("hz2LRUMap").get("2").equals(new Integer(2)));
-        Assert.assertTrue(hz2Instance.getMap("hz2LRUMap").get("b").equals("b"));
+        Assert.assertTrue(hz2Instance.getMap("hz1LRUMap").get("2").equals(new Integer(2)));
+        Assert.assertTrue(hz2Instance.getMap("hz2LRUMap").get("a").equals("a"));
+        Assert.assertTrue(hz1Instance.getMap("hz2LRUMap").get("b").equals("b"));
+        
+        // Queue test ----------------------------------------------------------
+        BlockingQueue<String> hz1Queue = hz1Instance.getQueue("hz1Queue");
+        try {
+            hz1Queue.put("hz1Queue-1");
+        } catch (InterruptedException ex) {
+            logger.error(ex.getMessage(), ex);
+            throw new RuntimeException(ex);
+        }
+        BlockingQueue<String> hz2Queue = hz2Instance.getQueue("hz1Queue");
+        try {
+            hz2Queue.put("hz1Queue-2");
+        } catch (InterruptedException ex) {
+            logger.error(ex.getMessage(), ex);
+            throw new RuntimeException(ex);
+        }
+        
+        // Assert queue
+        try {
+            Assert.assertEquals("hz1Queue-1", hz1Queue.take());
+            Assert.assertEquals("hz1Queue-2", hz1Queue.take());
+            Assert.assertEquals(true, hz1Queue.isEmpty());
+        } catch (InterruptedException ex) {
+            logger.error(ex.getMessage(), ex);
+            throw new RuntimeException(ex);
+        }
     }
 }
