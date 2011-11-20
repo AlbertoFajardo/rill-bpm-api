d772983c42ae6327f4ab74ec92acceb8372dce7e
totyumengr@gmail.com <totyumengr@gmail.com@441bde5e-0c81-691e-53e5-807c429adba5>
2011-11-20 14:05:11 +0000

    Refactor schema operation extension mechanism, but has not test OK.

---
+++ trunk/rill-bpm/rill-bpm-api/src/main/java/com/baidu/rigel/service/workflow/api/activiti/bpmndiagram/RiglWfTransitionTraceSchemaListener.java
@@ -0,0 +1,47 @@
+package com.baidu.rigel.service.workflow.api.activiti.bpmndiagram;
+
+import org.activiti.engine.impl.db.DbSqlSession;
+import org.activiti.engine.impl.interceptor.Command;
+import org.activiti.engine.impl.interceptor.CommandContext;
+import org.activiti.engine.impl.interceptor.CommandExecutor;
+
+import com.baidu.rigel.service.workflow.api.activiti.RillProcessEngineConfiguration.SchemaOperationEventListener;
+
+public class RiglWfTransitionTraceSchemaListener extends SchemaOperationEventListener {
+
+	@Override
+	public void onSchemaCreate(CommandExecutor commandExecutor, DbSqlSession dbSqlSession) {
+		
+		// RIGEL_WF_* DB initialize
+		commandExecutor.execute(new Command<Boolean>() {
+
+			public Boolean execute(CommandContext commandContext) {
+
+				boolean tablePresent = commandContext.getDbSqlSession()
+						.isTablePresent("RIGEL_WF_TRANSITION_TAKE_TRACE");
+				if (tablePresent) {
+					return true;
+				}
+
+				// Do create
+				String resourceName = getResourceForDbOperation(
+						commandContext.getDbSqlSession(), "create", "create",
+						"wf");
+				commandContext.getDbSqlSession().executeSchemaResource(
+						"create", "wf", resourceName, false);
+				return true;
+			}
+
+			String getResourceForDbOperation(DbSqlSession dbSqlSession,
+					String directory, String operation, String component) {
+				String databaseType = dbSqlSession.getDbSqlSessionFactory()
+						.getDatabaseType();
+				return "com/baidu/rigel/service/workflow/db/" + directory
+						+ "/rigel." + databaseType + "." + operation + "."
+						+ component + ".sql";
+			}
+		});
+        
+	}
+	
+}
