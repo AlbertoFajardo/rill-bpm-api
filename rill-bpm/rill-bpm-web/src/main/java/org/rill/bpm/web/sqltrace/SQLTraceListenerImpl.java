8a46458d50dfb3f8982e6f243b04368201b5ae52
totyumengr <totyumengr@gmail.com>
2012-02-01 08:02:55 +0000

    Enhancement - Add SQL trace listener

---
+++ rill-bpm/rill-bpm-web/src/main/java/org/rill/bpm/web/sqltrace/SQLTraceListenerImpl.java
@@ -0,0 +1,18 @@
+package org.rill.bpm.web.sqltrace;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.glassfish.api.jdbc.SQLTraceListener;
+import org.glassfish.api.jdbc.SQLTraceRecord;
+
+public class SQLTraceListenerImpl implements SQLTraceListener {
+
+	private static final Log LOGGER = LogFactory.getLog(SQLTraceListenerImpl.class);
+	
+	@Override
+	public void sqlTrace(SQLTraceRecord record) {
+		
+		LOGGER.debug(record);
+	}
+
+}
