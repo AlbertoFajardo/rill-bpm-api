dd3a637ef707539b387df9530b2714f7d76877f7
totyumengr@gmail.com <totyumengr@gmail.com@441bde5e-0c81-691e-53e5-807c429adba5>
2012-04-14 15:05:18 +0000

    Enhancement - Add scale-out PERSISTENT_CACHE manage interface.

--- trunk/rill-bpm/rill-bpm-web/src/main/java/org/rill/bpm/web/cachemgr/WorkflowCacheMgrController.java
+++ trunk/rill-bpm/rill-bpm-web/src/main/java/org/rill/bpm/web/cachemgr/WorkflowCacheMgrController.java
@@ -1,17 +1,25 @@
 package org.rill.bpm.web.cachemgr;
 
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map.Entry;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngines;
+import org.activiti.engine.history.HistoricProcessInstance;
 import org.activiti.engine.impl.util.json.JSONWriter;
+import org.rill.bpm.api.scaleout.ScaleoutHelper;
 import org.rill.bpm.web.ScaleoutControllerSupport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.Cache;
 import org.springframework.cache.Cache.ValueWrapper;
 import org.springframework.cache.CacheManager;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.Assert;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -88,14 +96,7 @@ public class WorkflowCacheMgrController extends ScaleoutControllerSupport {
 		
 		JSONWriter jsonWriter = new JSONWriter(out);
 		Cache cache = cacheManager.getCache(cacheName);
-		cache.put(cacheKey, new ValueWrapper(){
-
-			@Override
-			public Object get() {
-				return cacheValue;
-			}
-			
-		});
+		cache.put(cacheKey, cacheValue);
 		
 		jsonWriter
 			.object()
@@ -107,6 +108,58 @@ public class WorkflowCacheMgrController extends ScaleoutControllerSupport {
 			.value(cacheKey)
 			.key("cacheValue")
 			.value(ObjectUtils.getDisplayString(cache.get(cacheKey) == null ? null : cache.get(cacheKey).get()))
+			.endObject();
+		out.flush();
+		
+	}
+	
+	@RequestMapping(value = { "/{cacheName}/SCALEOUT_KEY" }, method = RequestMethod.PUT)
+	public void rebuildScaleout(HttpServletRequest request,
+			HttpServletResponse response, @PathVariable(value="cacheName") final String cacheName) throws Exception {
+		
+		// FIXME MENGRAN. only support PERSISTENT_CACHE
+		response.setContentType("application/json;charset=UTF-8");
+		final PrintWriter out = response.getWriter();
+		
+		Cache cache = cacheManager.getCache(cacheName);
+		Assert.notNull(cache, "Can not get cache by name " + cacheName);
+		
+		List<String> add = new ArrayList<String>();
+		List<String> update = new ArrayList<String>();
+		
+		for (Entry<String, ProcessEngine> entry : ProcessEngines.getProcessEngines().entrySet()) {
+			List<HistoricProcessInstance> list = entry.getValue().getHistoryService().createHistoricProcessInstanceQuery().unfinished().orderByProcessInstanceStartTime().asc().list();
+			LOGGER.info("--- Start rebuild scaleout key(" + list.size() + ") for " + entry.getKey());
+			for (HistoricProcessInstance hpi : list) {
+				if (hpi.getBusinessKey() != null) {
+					ValueWrapper vw = cache.get(ScaleoutHelper.generateScaloutKey(hpi.getBusinessKey()));
+					if (vw == null) {
+						LOGGER.info("Add scaleout key " + entry.getKey() + "'s hashcode for business key " + hpi.getBusinessKey());
+						cache.put(ScaleoutHelper.generateScaloutKey(hpi.getBusinessKey()), new Integer(entry.getKey().hashCode()).toString());
+						add.add(ScaleoutHelper.generateScaloutKey(hpi.getBusinessKey()) + "=" + new Integer(entry.getKey().hashCode()).toString());
+					} else {
+						if (new Integer(entry.getKey().hashCode()).toString().equals(vw.get().toString())) {
+							LOGGER.info("No-op scaleout key " + entry.getKey() + "'s hashcode for business key " + hpi.getBusinessKey());
+						} else {
+							LOGGER.info("Update scaleout key " + entry.getKey() + "'s hashcode for business key " + hpi.getBusinessKey());
+							String old = vw.get().toString();
+							cache.put(ScaleoutHelper.generateScaloutKey(hpi.getBusinessKey()), new Integer(entry.getKey().hashCode()).toString());
+							update.add(ScaleoutHelper.generateScaloutKey(hpi.getBusinessKey()) + "=" + new Integer(entry.getKey().hashCode()).toString() + " " + old);
+						}
+					}
+				}
+			}
+		}
+		
+		JSONWriter jsonWriter = new JSONWriter(out);
+		jsonWriter
+			.object()
+			.key("type")
+			.value("MIX")
+			.key("add")
+			.value(add)
+			.key("update")
+			.value(update)
 			.endObject();
 		out.flush();
 		
@@ -139,6 +192,5 @@ public class WorkflowCacheMgrController extends ScaleoutControllerSupport {
 		out.flush();
 		
 	}
-	
 	
 }
