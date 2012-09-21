bacb881c45d4043bdc63060a8a131f283a690148
totyumengr <totyumengr@441bde5e-0c81-691e-53e5-807c429adba5>
2012-09-21 12:00:39 +0000

    rill-analysis: InfinispanSessionCache

--- trunk/rill-analysis/rill-analysis-report/src/main/java/nu/com/rill/analysis/report/bo/Report.java
+++ trunk/rill-analysis/rill-analysis-report/src/main/java/nu/com/rill/analysis/report/bo/Report.java
@@ -29,7 +29,7 @@ public class Report implements Serializable {
 	
 	private String cronExpression;
 	
-	private byte[] reportContent;
+	private transient byte[] reportContent;
 	
 	private Date addDate;
 
