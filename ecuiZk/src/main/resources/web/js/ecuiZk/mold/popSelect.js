bdad010f08b6f8e1c9696da317d0c085f58f7743
denghongqi <denghongqi@gmail.com>
2012-10-10 09:24:17 +0000

    修改多选框弹出浮层位置错误的问题

--- ecuiZk/src/main/resources/web/js/ecuiZk/mold/popSelect.js
+++ ecuiZk/src/main/resources/web/js/ecuiZk/mold/popSelect.js
@@ -13,16 +13,22 @@ function (out) {
 	this._popButId = this._ecuiId + "-but";
 	this._popPanelId = this._ecuiId + "-panel";
 
-	out.push("<div style='position:relative;display:inline-block;*zoom:1;_display:inline'><span ecui='type:pop-select;id:", this._popButId, ";target:", this._popPanelId, "'");
+	out.push("<span ecui='type:pop-select;id:", this._popButId, ";target:", this._popPanelId, "'");
 	this._options.butWidth && (out.push(" style='width:", this._options.butWidth, "'"));
 	out.push("></span>");
 	
-	out.push("<div ecui='type:pop;id:", this._popPanelId, "' style='width:", this._options.panelWidth, "'>");
+	var html;
 	for (var i = 0, o; o = this._items[i++]; ){
 		var optStr = "value:" + o[0] + ";";
         optStr += "checked:" + ((o[2] === "true" || o[2] === true) ? "true" : "false") + ";";
         optStr += "def-checked:" + ((o[3] === "true" || o[3] === true) ? "true" : "false");
-        out.push("<span ecui='", optStr, "'>", o[1], "</span>");
+        html += "<span ecui='" + optStr + "'>" + o[1] + "</span>";
 	}
-	out.push("</div></div>");
+	
+	var pop = document.createElement("div");
+	pop.innerHTML = html;
+	pop.setAttribute("ecui", "type:pop-select; id:" + this._popPanelId);
+	pop.setAttribute("style", "width:" + this._options.panelWidth);
+	
+	document.body.appendChild(pop);
 }
