74cbcf9d68e5bad68037ba83604e69943599c718
totyumengr <totyumengr@441bde5e-0c81-691e-53e5-807c429adba5>
2012-10-15 09:38:49 +0000

    rill-analysis: 0.1.0 release

--- trunk/rill-analysis/rill-analysis-report-core/src/main/java/nu/com/rill/analysis/report/excel/ConditionalFormattingHelper.java
+++ trunk/rill-analysis/rill-analysis-report-core/src/main/java/nu/com/rill/analysis/report/excel/ConditionalFormattingHelper.java
@@ -4,6 +4,8 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.openxmlformats.schemas.spreadsheetml.x2006.main.CTCfRule;
+import org.openxmlformats.schemas.spreadsheetml.x2006.main.CTCfvo;
 import org.openxmlformats.schemas.spreadsheetml.x2006.main.CTColor;
 import org.openxmlformats.schemas.spreadsheetml.x2006.main.CTDxf;
 import org.springframework.util.Assert;
@@ -13,9 +15,37 @@ import org.zkoss.poi.ss.usermodel.ConditionalFormatting;
 import org.zkoss.poi.ss.usermodel.Sheet;
 import org.zkoss.poi.ss.util.CellRangeAddress;
 import org.zkoss.poi.xssf.usermodel.XSSFConditionalFormattingRule;
+import org.zkoss.zss.model.Worksheet;
+import org.zkoss.zss.ui.impl.Utils;
 
 public class ConditionalFormattingHelper {
-
+	
+	private static void processIconSet(Map<String, String> result, XSSFConditionalFormattingRule rule, Cell cell) {
+		
+		CTCfRule ctRule = rule.getCTCfRule();
+		if (!ctRule.isSetIconSet() && ctRule.getIconSet().xgetIconSet().getStringValue().equals("3Arrows")) {
+			return;
+		}
+		
+		CTCfvo vo = ctRule.getIconSet().getCfvoList().get(0);
+		result.put("background-repeat", "no-repeat");
+		int width = Utils.getColumnWidthInPx((Worksheet) cell.getSheet(), cell.getColumnIndex());
+		int height = Utils.getRowHeightInPx((Worksheet) cell.getSheet(), cell.getRow());
+		result.put("padding-left", (width * 1.5) + "px");
+		result.put("background-position-y", (height -10) / 2 + "px");
+		if (new Double(vo.getVal()).compareTo(cell.getNumericCellValue()) < 0) {
+			// Up
+			String path = "./images/arrow-up.png";
+			result.put("background-image", "url('" + path + "')");
+		}
+		if (new Double(vo.getVal()).compareTo(cell.getNumericCellValue()) > 0) {
+			// Down
+			String path = "./images/arrow-down.png";
+			result.put("background-image", "url('" + path + "')");
+		}
+		
+	}
+	
 	public static Map<String, String> getCFInWorkbook(Sheet sheet, Cell cell) {
 		
 		Assert.notNull(sheet);
@@ -34,26 +64,34 @@ public class ConditionalFormattingHelper {
 				ConditionalFormatting cf = sheet.getSheetConditionalFormatting().getConditionalFormattingAt(j);
 				for (int r = 0; r < cf.getNumberOfRules(); r++) {
 					XSSFConditionalFormattingRule rule = (XSSFConditionalFormattingRule) cf.getRule(r);
-					CTDxf dxf = rule.getDxf(false);
-					if (dxf.getFont() != null) {
-						CTColor color = dxf.getFont().getColorArray(0);
-						String fontRgb = color.xgetRgb().getStringValue();
-						result.put("color", "#" + fontRgb.substring(2));
-					}
-					if (dxf.getFill() != null) {
-						String bgRgb = null;
-						bgRgb = dxf.getFill().getPatternFill().getBgColor().xgetRgb().getStringValue();
-						result.put("background-color", "#" + bgRgb.substring(2));
-					}
+					
 					for (CellRangeAddress address : cf.getFormattingRanges()) {
 						boolean isInRange = address.isInRange(cell.getRowIndex(), cell.getColumnIndex());
 						if (isInRange) {
-							if (ComparisonOperator.LT == rule.getComparisonOperation() && 
-									new Double(rule.getFormula1()).compareTo(cell.getNumericCellValue()) > 0) {
-								return result;
-							}
-							if (ComparisonOperator.GT == rule.getComparisonOperation() && 
-									new Double(rule.getFormula1()).compareTo(cell.getNumericCellValue()) < 0) {
+							CTDxf dxf = rule.getDxf(false);
+							if (dxf != null) {
+								if (dxf.getFont() != null) {
+									CTColor color = dxf.getFont().getColorArray(0);
+									String fontRgb = color.xgetRgb().getStringValue();
+									result.put("color", "#" + fontRgb.substring(2));
+								}
+								if (dxf.getFill() != null) {
+									String bgRgb = null;
+									bgRgb = dxf.getFill().getPatternFill().getBgColor().xgetRgb().getStringValue();
+									result.put("background-color", "#" + bgRgb.substring(2));
+								}
+								if (ComparisonOperator.LT == rule.getComparisonOperation() && 
+										new Double(rule.getFormula1()).compareTo(cell.getNumericCellValue()) > 0) {
+									return result;
+								}
+								if (ComparisonOperator.GT == rule.getComparisonOperation() && 
+										new Double(rule.getFormula1()).compareTo(cell.getNumericCellValue()) < 0) {
+									return result;
+								}
+							} else {
+								// iconSet processing
+								processIconSet(result, rule, cell);
+								
 								return result;
 							}
 						}
