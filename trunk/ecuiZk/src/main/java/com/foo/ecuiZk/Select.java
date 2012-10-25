6ff5489ce1423f65153b0ea37efbf79aac80f093
totyumengr <totyumengr@441bde5e-0c81-691e-53e5-807c429adba5>
2012-10-25 15:43:09 +0000

    ecuiZk: 0.1.0 release // Enhangce change event: synchronize text and items

--- trunk/ecuiZk/src/main/java/com/foo/ecuiZk/Select.java
+++ trunk/ecuiZk/src/main/java/com/foo/ecuiZk/Select.java
@@ -1,16 +1,13 @@
 package com.foo.ecuiZk;
 
-import java.util.Map;
-import java.util.HashMap;
 import java.util.ArrayList;
-
-import org.zkoss.zk.ui.Component;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.zkoss.lang.Objects;
 import org.zkoss.zk.au.AuRequest;
 import org.zkoss.zk.ui.event.Event;
 import org.zkoss.zk.ui.event.Events;
-import org.zkoss.zk.ui.event.ForwardEvent;
 import org.zkoss.zul.impl.XulElement;
 
 public class Select extends XulElement {
@@ -22,6 +19,11 @@ public class Select extends XulElement {
 	*/
 	
 	/* Here's a simple example for how to implements a member field */
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	private String _text;
 	
@@ -50,14 +52,22 @@ public class Select extends XulElement {
 	public void setText(String text) {
 		if (!Objects.equals(_text, text)) {
 			_text = text;
-			smartUpdate("text", _text);
+//			smartUpdate("text", _text);
 		}
 	}
 	
 	public void setValue(String value) {
 		if (!Objects.equals(_value, value)) {
 			_value = value;
-			smartUpdate("value", _value);
+//			smartUpdate("value", _value);
+			
+			// Add by MENGRAN for synchronize value and text
+			for (HashMap<String, String> map : getItems()) {
+				if (map.get("value").equals(_value)) {
+					setText(map.get("text"));
+					break;
+				}
+			}
 		}
 	}
 	
@@ -69,6 +79,14 @@ public class Select extends XulElement {
 	public void setItems(ArrayList<HashMap<String, String>> items) {
 		_items = items;
 		smartUpdate("items", _items);
+		
+		// Add by MENGRAN for synchronize value and items
+		for (HashMap<String, String> map : items) {
+			if (map.containsKey("selected")) {
+				setValue(map.get("value"));
+				break;
+			}
+		}
 	}
 	
 	static {
@@ -85,6 +103,7 @@ public class Select extends XulElement {
 		render(renderer, "items", _items);
 	}
 	
+	@SuppressWarnings("rawtypes")
 	public void service(AuRequest request, boolean everError) {
 		final String cmd = request.getCommand();
 		final Map data = request.getData();
@@ -96,7 +115,7 @@ public class Select extends XulElement {
 		} 
 		else if (cmd.equals("onChange")) {
 			final String value = (String)data.get("value");
-			_value = value;
+			this.setValue(value);
 			//System.out.println(_value);
 			Events.postEvent(Event.getEvent(request));
 		}else
