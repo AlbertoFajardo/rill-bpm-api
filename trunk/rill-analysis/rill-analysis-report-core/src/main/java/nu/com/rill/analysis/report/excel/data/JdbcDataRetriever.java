5a59500f99388f0bb6b70f861dc5483c46f5a05e
totyumengr@gmail.com <totyumengr@gmail.com@441bde5e-0c81-691e-53e5-807c429adba5>
2012-10-30 02:04:13 +0000

    rill-analysis: Complete code of jdbc support.

--- trunk/rill-analysis/rill-analysis-report-core/src/main/java/nu/com/rill/analysis/report/excel/data/JdbcDataRetriever.java
+++ trunk/rill-analysis/rill-analysis-report-core/src/main/java/nu/com/rill/analysis/report/excel/data/JdbcDataRetriever.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
+import java.util.WeakHashMap;
 
 import javax.sql.DataSource;
 
@@ -11,10 +13,13 @@ import nu.com.rill.analysis.report.REException;
 import nu.com.rill.analysis.report.excel.DataRetriever;
 import nu.com.rill.analysis.report.excel.ReportEngine;
 
+import org.apache.commons.dbcp.BasicDataSourceFactory;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.type.TypeReference;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.util.Assert;
 import org.zkoss.poi.ss.usermodel.Cell;
 import org.zkoss.poi.ss.usermodel.Row;
 import org.zkoss.zss.model.Worksheet;
@@ -23,20 +28,60 @@ public class JdbcDataRetriever implements DataRetriever {
 
 	public final Log LOGGER = LogFactory.getLog(this.getClass());
 	
+	private Map<String, DataSource> map = new WeakHashMap<String, DataSource>();
+	
+	private Properties dbcpProperties;
+	
+	public final Properties getDbcpProperties() {
+		return dbcpProperties;
+	}
+
+	public final void setDbcpProperties(Properties dbcpProperties) {
+		this.dbcpProperties = dbcpProperties;
+	}
+
 	@Override
 	public boolean supportType(DATA_TYPE dt) {
 		
 		return DATA_TYPE.jdbc.equals(dt);
 	}
 	
-	protected DataSource retrieveDataSource(String url, String userName, String password) {
+	protected synchronized DataSource retrieveDataSource(String url, String userName, String password) {
+		
+		Assert.notNull(url);
+		Assert.notNull(userName);
+		Assert.notNull(password);
 		
 		// FIXME: MENGRAN. Extension point here. Simply we can construct data source, 
 		// or we should hold a key-value pair to quickly retrieve action.
+		for (int i = 0; i < 3 ; i++) {
+			String key = url + userName + password;
+			DataSource dataSource = map.get(key);
+			if (dataSource == null) {
+				Properties useProperties = new Properties();
+				useProperties.putAll(getDbcpProperties());
+				useProperties.put("url", url);
+				useProperties.put("username", userName);
+				useProperties.put("password", password);
+				try {
+					dataSource = BasicDataSourceFactory.createDataSource(useProperties);
+					map.put(key, dataSource);
+					LOGGER.debug("Construct a data source(dbcp) and record it. " + url + " username " + userName);
+				} catch (Exception e) {
+					LOGGER.error("Error to construct dbcp datasource." + url + " " + "username " + userName, e);
+					throw new REException("无法建立数据库连接。");
+				}
+			}
+			
+			dataSource = map.get(key);
+			if (dataSource == null) {
+				LOGGER.info("Can not retrieve datasource, we try again. " + i);
+			} else {
+				return dataSource;
+			}
+		}
 		
-		
-		
-		return null;
+		throw new REException("无法连接数据库（重试3次）。");
 	}
 
 	@SuppressWarnings("unchecked")
@@ -46,7 +91,10 @@ public class JdbcDataRetriever implements DataRetriever {
 		String url = reportParams.get(ReportEngine.URL);
 		String userName = reportParams.get(ReportEngine.USERNAME);
 		String password = reportParams.get(ReportEngine.PASSWORD);
+		DataSource dataSource = retrieveDataSource(url, userName, password);
+		JdbcTemplate template = new JdbcTemplate(dataSource);
 		
+		// Start execute SQL to retrieve result.
 		Row row = dataSheet.getRow(dataSheet.getFirstRowNum());
 //		String url = row.getCell(row.getFirstCellNum()).getStringCellValue();
 		String result = ReportEngine.fetchUrl(reportParams.get(ReportEngine.URL) + url, reportParams);
