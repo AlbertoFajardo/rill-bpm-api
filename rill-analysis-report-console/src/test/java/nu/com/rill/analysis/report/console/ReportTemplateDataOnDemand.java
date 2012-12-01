727de5bf8c189d1015980f04778617f23bbc8591
totyumengr@gmail.com <totyumengr@gmail.com@441bde5e-0c81-691e-53e5-807c429adba5>
2012-12-01 08:15:13 +0000

    delete rill analysis report console

--- rill-analysis-report-console/src/test/java/nu/com/rill/analysis/report/console/ReportTemplateDataOnDemand.java
+++
@@ -1,7 +0,0 @@
-package nu.com.rill.analysis.report.console;
-
-import org.springframework.roo.addon.dod.RooDataOnDemand;
-
-@RooDataOnDemand(entity = ReportTemplate.class)
-public class ReportTemplateDataOnDemand {
-}
