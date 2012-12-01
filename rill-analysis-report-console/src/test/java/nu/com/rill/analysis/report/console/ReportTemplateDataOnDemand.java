21d43997aa7d2c9407df502d67b506852c340c50
totyumengr@gmail.com <totyumengr@gmail.com@441bde5e-0c81-691e-53e5-807c429adba5>
2012-12-01 07:24:00 +0000

    Share project "rill-analysis-report-console" into "https://rill-bpm-api.googlecode.com/svn"

---
+++ rill-analysis-report-console/src/test/java/nu/com/rill/analysis/report/console/ReportTemplateDataOnDemand.java
@@ -0,0 +1,7 @@
+package nu.com.rill.analysis.report.console;
+
+import org.springframework.roo.addon.dod.RooDataOnDemand;
+
+@RooDataOnDemand(entity = ReportTemplate.class)
+public class ReportTemplateDataOnDemand {
+}
